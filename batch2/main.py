4b518acafcb7a1bbbc5211c50765d72dd9d3d0f0
KeerthikaR-01 <keerthika21201@gmail.com>
2026-05-25 15:13:20 +0530

    implement add user, print_users and get_user_email

--- batch2/main.py
+++ batch2/main.py
@@ -3,8 +3,12 @@ from utils.inputs import get_choice
 from utils.common import clear_console
 from ui.create_user import create_user
 from utils.logger import log_info_message
+from repository.UserRepository import UserRepository
+from ui.print_users import print_users
 
 def main():
+    userRepository = UserRepository()
+
     while True:
         print_main_menu()
 
@@ -14,12 +18,21 @@ def main():
             
             new_user = create_user()
 
-            log_info_message(f"User created Successfully: Name: {new_user.name}, Email: {new_user.email}, Password: {new_user.password}")
+            if userRepository.get_user_email(new_user.email):
+                log_info_message(f"Email {new_user.email} already exists.")
+                continue
+
+            user_id = len(userRepository.users) + 1
+            new_user.id = user_id
+
+            userRepository.add_user(new_user)
+
+            log_info_message(f"User created Successfully: ID: {new_user.id}, Name: {new_user.name}, Email: {new_user.email}, Password: {new_user.password}")
             continue
 
         elif choice == 2:
-            clear_console()
-            print("View Users")
+            users = userRepository.get_users()
+            print_users(users)
             continue
 
         elif choice == 3:
---
+++ batch2/ui/print_users.py
@@ -0,0 +1,10 @@
+from utils.common import clear_console
+from utils.ui import print_menu_title
+
+def print_users(users):
+    clear_console()
+
+    print_menu_title("View Users")
+
+    for user in users:
+        print(f"ID: {user.id}, Name: {user.name}, Email: {user.email}, Password: {user.password}")
