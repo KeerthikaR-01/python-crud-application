81247d0560c71ebf95e6731205174979d4d1e246
Keerthika <keerthika@pyroferus.com>
2026-06-13 10:09:30 +0530

    Deleted folder batch 2

--- batch2/utils/inputs.py
+++
@@ -1,6 +0,0 @@
-def get_choice(message = "Enter your choice: "):
-    choice = input(message)
-    return int(choice)
-
-def get_input(message):
-    return input(message)
--- batch2/utils/logger.py
+++
@@ -1,5 +0,0 @@
-def log_error_message(message):
-    print(f"Error: {message}")
-
-def log_info_message(message):
-    print(f"Info: {message}")
